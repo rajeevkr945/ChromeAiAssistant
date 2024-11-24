bdf023c333a5b804961904982dac113115bbba84
rajeev k raveendran <rajeevkr945@gmail.com>
2024-11-25 01:20:33 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 document.addEventListener("mouseup", () => {
   const selectedText = window.getSelection().toString().trim();
-  console.log("Selected text:", selectedText);  // Log the selected text for debugging
+  console.log("Selected text:", selectedText);  // debuging inu ulla logs
   if (selectedText) {
     chrome.runtime.sendMessage({ text: selectedText });
   }
